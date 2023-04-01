6af8529bd052260e995bdded6443ca5b7494d747
猫猫摸大鱼 <58991169+miaowmint@users.noreply.github.com>
2023-04-01 23:28:27 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -14,10 +14,12 @@ echo '|_| |_| |_|_|\__,_|\___/ \_/\_/ |_| |_| |_|_|_| |_|\__|'
 echo '                                                       '
 
 source /usr/local/qexo/shenv.sh
-if [ "$WAS_EXECUTED" != "yes" ];then
-    echo -e "\033[34m首次运行，开始配置nginx并使用acme.sh申请证书\033[0m"
-    echo "export WAS_EXECUTED='yes'" > /usr/local/qexo/shenv.sh
-    sleep 5s
+
+runqexo(){
+    cd /usr/local/qexo && python manage.py makemigrations && python manage.py migrate && python manage.py runserver 0.0.0.0:8000 --noreload
+}
+
+firstrun(){
     sudo cp -f /usr/local/qexo/nginx1.conf /etc/nginx/nginx.conf && sudo service nginx restart
     echo "ACCOUNT_EMAIL='$ACMEMAIL'" >> /root/.acme.sh/account.conf
     acme.sh --upgrade --auto-upgrade && acme.sh  --register-account  -m $ACMEMAIL
@@ -25,10 +27,21 @@ if [ "$WAS_EXECUTED" != "yes" ];then
     acme.sh  --issue  -d $QEXODOMAIN  --nginx /etc/nginx/nginx.conf
     acme.sh  --issue  -d $HEXODOMAIN  --nginx /etc/nginx/nginx.conf
     sudo cp -f /usr/local/qexo/nginx2.conf /etc/nginx/nginx.conf && sudo service nginx restart
-    cd /usr/local/qexo && python manage.py makemigrations && python manage.py migrate && python manage.py runserver 0.0.0.0:8000 --noreload
+    echo "export WAS_EXECUTED='yes'" > /usr/local/qexo/shenv.sh
+    runqexo
+}
+if [ "$USEPROXY" != "yes" ];then
+    echo -e "\033[34m开始运行qexo\033[0m"
+    runqexo
 else
-    echo -e "\033[34m检测到配置已完成，开始重启nginx并运行qexo\033[0m"
-    sleep 2s
-    sudo service nginx restart
-    cd /usr/local/qexo && python manage.py makemigrations && python manage.py migrate && python manage.py runserver 0.0.0.0:8000 --noreload
+    if [ "$WAS_EXECUTED" != "yes" ];then
+        echo -e "\033[34m首次运行，开始配置nginx并使用acme.sh申请证书\033[0m"
+        sleep 5s
+        firstrun
+    else
+        echo -e "\033[34m检测到配置已完成，开始重启nginx并运行qexo\033[0m"
+        sleep 2s
+        sudo service nginx restart
+        runqexo
+    fi
 fi
