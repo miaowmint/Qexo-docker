8b774c94648e83a77d4d40d033e4c244b14c5809
猫猫摸大鱼 <58991169+miaowmint@users.noreply.github.com>
2023-04-02 00:19:58 +0800

    Update qexoinstall.sh

--- qexoinstall.sh
+++ qexoinstall.sh
@@ -12,13 +12,14 @@ echo '| '"'"'_ ` _ \| |/ _` |/ _ \ \ /\ / / '"'"'_ ` _ \| | '"'"'_ \| __|'
 echo '| | | | | | | (_| | (_) \ V  V /| | | | | | | | | | |_ '
 echo '|_| |_| |_|_|\__,_|\___/ \_/\_/ |_| |_| |_|_|_| |_|\__|'
 echo '                                                       '
-echo -e "\033[34m欢迎使用Qexo-docker一键安装脚本v2.6.3.3，如果此脚本安装出现错误请进行手动部署\033[0m"
+echo -e "\033[34m欢迎使用Qexo-docker一键安装脚本v2.6.4.1(04.02)，如果此脚本安装出现错误请进行手动部署\033[0m"
 sleep 5s
 
 echoinfo(){
     echo -e "\n\033[31m如果出现权限问题，请使用ROOT权限运行此脚本\033[0m"
     echo "此脚本要做的事：对一些配置项进行设置->检测是否安装Docker及git->下载并修改所需文件并按需运行Docker容器"
-    echo -e "此脚本虽会询问你要使用的域名，但仅用于修改configs.py配置文件，\033[31m如需要进行反代请自行手动操作\033[0m"
+    echo "此脚本中提到的 hexo 特指qexo支持的所有静态博客，不止局限于hexo"
+    echo "此脚本中部分配置是固定的，如需修改请通过源码 otherinfo 处自行修改"
     echo -e "\033[33m先休息10秒，如需停止运行此脚本请按 Ctrl + C 退出\033[0m"
     sleep 10s
     echo -e "\n\033[33m我们先来进行一些配置\033[0m"
@@ -44,39 +45,43 @@ checkport(){
 }
 
 basicinfo(){
-    read -p " 请输入你要用来反代的域名，回车默认为不使用域名反代Qexo" domain
-        if [ -z "$domain" ];then
-            domain="yoursite.com"
+    # read -p " 你的hexo是否部署在本地？回车默认为部署在本地，输入任意字符视为部署在github等托管平台 " locally
+    #     if [ -z "$locally" ];then
+    #         locally="yes"
+    #     fi
+    echo -e "\n\033[31m此脚本默认你的hexo部署在本地，如果你的hexo部署在github等托管平台，建议你使用Vercel部署，参考 https://www.oplog.cn/qexo/start/build.html （其实是因为逻辑太麻烦我懒得写）\033[0m"
+    echo -e "你是否需要镜像内置的nginx来反代qexo和hexo？（反代后可以通过域名访问qexo和hexo，\n但如果你的服务器已经安装了nginx等web服务端，或是后续有建其他网站的需求，建议你在服务器上自行反代而不是用镜像内置的nginx反代）"
+    read -p " 请注意，回车默认为使用镜像内置的nginx反代，输入任意字符视为无需反代或自行反代 " useproxy
+        if [ -z "$useproxy" ];then
+            useproxy="yes"
         fi
-    echo -e "\033[31m再次提示此脚本不负责配置反代，如需要进行反代请自行手动操作\033[0m"    
-    read -p " 如果你的博客部署在本地，那么请输入你博客源码的绝对路径，留空则视为博客部署在GithubPages等非本地位置" hexofile
+    read -p " 请输入你的qexo使用的域名，回车默认为不需要使用域名反代Qexo " qexodomain
+        if [ -z "$qexodomain" ];then
+            qexodomain="yourqexo.com"
+        fi 
+    read -p " 请输入你hexo源码的绝对路径 " hexofile
+        if [ -z "$hexofile" ];then
+            hexofile="/www/site"
+        fi 
     read -p " 接下来的配置项非必须项，是否跳过它们？回车默认跳过以一键安装，输入任意字符串以继续配置" oneclick
-        if [ "$oneclick" != "" ];then
-            qexoinfo
-        else
-            checkport
-            qexoname="qexo"
-            qexofile="/data/qexo"
-            qexoport=$portwillbe
+        if [ "$useproxy" == "yes" ];then
+            read -p " 请输入你的hexo使用的域名 " hexodomain
+                if [ -z "$hexodomain" ];then
+                    hexodomain="yourhexo.com"
+                fi             
+            read -p " 请输入你的邮箱（用于申请ssl证书） " acmemail
+                if [ -z "$acmemail" ];then
+                    acmemail="example@qexo.com"
+                fi 
         fi
+    otherinfo
 }
 
-qexoinfo(){
-    read -p " 请输入Qexo容器的名称，回车默认 qexo " qexoname
-        if [ -z "$qexoname" ];then
-            qexoname="qexo"
-        fi
-    read -p " 请输入Qexo容器文件的储存位置，回车默认 /data/qexo " qexofile
-        if [ -z "$qexofile" ];then
-            qexofile="/data/qexo"
-        fi
-    read -p " 请输入Qexo容器的映射端口，回车默认映射到宿主机 8000 端口，如果自定义端口请确保你填的端口没有被占用 " qexoport
-        if [ -z "$qexoport" ];then
-            checkport
-            qexoport=$portwillbe
-        fi
-    echo -e "\033[33m配置完成了，休息10秒钟\033[0m"
-    sleep 10s
+otherinfo(){
+    checkport
+    qexoname="qexo"
+    qexofile="/data/qexo"
+    qexoport=$portwillbe
 }
 
 chickandinstall(){
@@ -117,12 +122,18 @@ downloadandchangefile(){
     echo -e "\033[33m接下来开始修改配置文件\033[0m"
     sleep 1s
     git clone https://ghproxy.com/https://github.com/Qexo/Qexo.git $qexofile
-    cd $qexofile && wget https://ghproxy.com/https://raw.githubusercontent.com/miaowmint/Qexo-docker/main/configs.py && mkdir -p $qexofile/db && cd db && wget https://ghproxy.com/https://raw.githubusercontent.com/miaowmint/Qexo-docker/main/db.sqlite3
+    cd $qexofile && wget https://ghproxy.com/https://github.com/miaowmint/Qexo-docker/releases/download/1.0.1/Qexo-docker1.0.1.tar.gz && tar zxvf Qexo-docker1.0.1.tar.gz && rm Qexo-docker1.0.1.tar.gz
     serverip=`curl -L www.loliapi.com/qexoip/`
     echo -e "获取到服务器的IP为\033[32m$serverip\033[0m"
     sed -i "s|127.0.0.1|$serverip|g" $qexofile/configs.py
-    sed -i "s|yoursite.com|$domain|g" $qexofile/configs.py
-    echo -e "\033[33mconfigs.py配置文件修改完成，休息5秒钟\033[0m"
+    sed -i "s|yourqexo.com|$qexodomain|g" $qexofile/configs.py
+    sed -i "s|yourqexo.com|$qexodomain|g" $qexofile/nginx1.conf
+    sed -i "s|yourhexo.com|$hexodomain|g" $qexofile/nginx1.conf
+    sed -i "s|/www/site|$hexofile|g" $qexofile/nginx1.conf
+    sed -i "s|yourqexo.com|$qexodomain|g" $qexofile/nginx2.conf
+    sed -i "s|yourhexo.com|$hexodomain|g" $qexofile/nginx2.conf
+    sed -i "s|/www/site|$hexofile|g" $qexofile/nginx2.conf
+    echo -e "\033[33m配置文件修改完成，休息5秒钟\033[0m"
     sleep 5s
 }
 
@@ -130,7 +141,11 @@ runcontainer(){
     downloadandchangefile
     echo -e "\033[33m接下来开始运行Qexo\033[0m"
     sleep 1s
-    docker run -dit --name $qexoname --hostname $qexoname --restart always -v $qexofile:/usr/local/qexo -v $hexofile:$hexofile -p $qexoport:8000 miaowmint/qexo-docker:2.6.3.1
+    if [ "$useproxy" == "yes" ];then
+    docker run -dit --name $qexoname --hostname $qexoname --restart always -v $qexofile:/usr/local/qexo -v $hexofile:$hexofile -p 80:80 -p 443:443 -e USEPROXY=$useproxy -e QEXODOMAIN=$qexodomain -e HEXODOMAIN=$hexodomain -e ACMEMAIL=$acmemail miaowmint/qexo-docker:latest
+    else
+    docker run -dit --name $qexoname --hostname $qexoname --restart always -v $qexofile:/usr/local/qexo -v $hexofile:$hexofile -p $qexoport:8000 miaowmint/qexo-docker:latest
+    fi
     echo -e "\033[33m完成，休息5秒钟\033[0m"
     sleep 5s
 }
@@ -139,19 +154,18 @@ echoendinfo(){
     echo -e "\n\033[34m====================================================================================================\033[0m"
     echo -e "\n\033[33m恭喜你，安装完成了！\033[0m"
     echo -e "\033[33m你现在可以访问 http://$serverip:$qexoport/ 进行初始化并愉快的使用Qexo了（记得在安全组/防火墙放通$qexoport端口）\033[0m"
-    if [ "$domain" != "yoursite.com" ];then
-        echo -e "\033[33m如果你已经设置域名反代到 http://127.0.0.1:$qexoport/ ，那么你就可以访问 http://$domain/ 进行初始化并愉快的使用Qexo了\033[0m"
+    if [ "$qexodomain" != "yourqexo.com" ];then
+        sleep 5s
+        echo -e "\033[33m如果你使用了镜像内置的nginx反代或已经自行设置域名反代到 http://127.0.0.1:$qexoport/ ，那么你就可以访问 http://$qexodomain/ 进行初始化并愉快的使用Qexo了\033[0m"
     fi
-    echo -e "\033[33m关于Qexo的初始化配置可参考 https://www.oplog.cn/qexo/configs/provider.html/ \033[0m"
+    if [ "$hexodomain" != "yourhexo.com" ];then
+        sleep 5s
+        echo -e "\033[33m如果你在 $hexofile 上传了hexo源码，那你现在就可以访问 http://$hexodomain/ 访问自己的博客了\033[0m"
+    fi
+    echo -e "\033[33m关于Qexo的初始化配置可参考 https://www.oplog.cn/qexo/configs/provider.html \033[0m"
     echo -e "\n\033[34m====================================================================================================\033[0m"
     sleep 2s
     echo -e "\n\033[33m你可以运行 docker logs $qexoname 以查看Qexo容器的运行日志\033[0m"
-    echo -e "\033[33m如果日志末尾如下所示，则说明容器运行正常\033[0m"
-    echo -e "\nSystem check identified no issues (0 silenced)."
-    echo "February xx, 20xx - xx:xx:xx"
-    echo "Django version 3.2.16, using settings 'core.settings'"
-    echo "Starting development server at http://0.0.0.0:8000/"
-    echo "Quit the server with CONTROL-C."
     echo -e "\n\033[33m如果Qexo使用有问题，请在此提交issue: https://github.com/Qexo/Qexo/issues/new/choose/ \033[0m"
     echo -e "\n\033[33m如果本一键脚本使用有问题，请在此提交issue: https://github.com/miaowmint/Qexo-docker/issues/new/ \033[0m"
 }
