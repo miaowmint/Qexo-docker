11ebd16c8953a11db47997aabb3cab01b45ef278
猫猫摸大鱼 <58991169+miaowmint@users.noreply.github.com>
2024-01-25 15:04:01 +0800

    Update qexoinstall.sh

--- qexoinstall.sh
+++ qexoinstall.sh
@@ -1,187 +1,275 @@
 #!/bin/bash
 
-# echo "           _                               _       _   "
-# echo " _ __ ___ (_) __ _  _____      ___ __ ___ (_)_ __ | |_ "
-# echo "| '_ ` _ \| |/ _` |/ _ \ \ /\ / / '_ ` _ \| | '_ \| __|"
-# echo "| | | | | | | (_| | (_) \ V  V /| | | | | | | | | | |_ "
-# echo "|_| |_| |_|_|\__,_|\___/ \_/\_/ |_| |_| |_|_|_| |_|\__|"
-# echo "                                                       "
-echo '           _                               _       _   '
-echo ' _ __ ___ (_) __ _  _____      ___ __ ___ (_)_ __ | |_ '
-echo '| '"'"'_ ` _ \| |/ _` |/ _ \ \ /\ / / '"'"'_ ` _ \| | '"'"'_ \| __|'
-echo '| | | | | | | (_| | (_) \ V  V /| | | | | | | | | | |_ '
-echo '|_| |_| |_|_|\__,_|\___/ \_/\_/ |_| |_| |_|_|_| |_|\__|'
-echo '                                                       '
-echo -e "\033[34m欢迎使用Qexo-docker一键安装脚本v2023.05.18.1，如果此脚本安装出现错误请进行手动部署\033[0m"
-sleep 5s
 
-echoinfo(){
-    echo -e "\n\033[31m如果出现权限问题，请使用ROOT权限运行此脚本\033[0m"
-    echo "此脚本要做的事：对一些配置项进行设置->检测是否安装Docker及git->下载并修改所需文件并按需运行Docker容器"
-    echo "此脚本中提到的 hexo 特指qexo支持的所有静态博客，不止局限于hexo"
-    echo "此脚本中部分配置是固定的，如需修改请通过源码 otherinfo 处自行修改"
-    echo -e "\033[33m先休息10秒，如需停止运行此脚本请按 Ctrl + C 退出\033[0m"
-    sleep 10s
-    echo -e "\n\033[33m我们先来进行一些配置\033[0m"
-    sleep 1s
+#  _ __ ___ (_) __ _  _____      ___ __ ___ (_)_ __ | |_ 
+# | '_ ` _ \| |/ _` |/ _ \ \ /\ / / '_ ` _ \| | '_ \| __|
+# | | | | | | | (_| | (_) \ V  V /| | | | | | | | | | |_ 
+# |_| |_| |_|_|\__,_|\___/ \_/\_/ |_| |_| |_|_|_| |_|\__|
+
+Green="\033[32m"
+Font="\033[0m"
+Red="\033[31m" 
+
+#root权限
+root_need(){
+    if [[ $EUID -ne 0 ]]; then
+        echo -e "${Red}请使用root权限运行此脚本，具体指令为 sudo -i ${Font}"
+        exit 1
+    fi
 }
 
-checkport(){
-    echo "检测8000端口是否被占用"
-    sleep 1s
-    portuse=`/usr/sbin/lsof -i :8000|grep -v "PID" | awk '{print $2}'`
-    sleep 1s
-    if [ $? -ne 0 ]; then
-        if [ "$portuse" != "" ];then
-            echo -e "\033[31m检测到8000端口已被占用，将Qexo映射端口修改到18000\033[0m"
-            portwillbe="18100"
+#检测ovz
+ovz_no(){
+    if [[ -d "/proc/vz" ]]; then
+        echo -e "${Red}不支持OpenVZ虚拟化VPS${Font}"
+        exit 1
+    fi
+}
+
+#检测并安装Docker
+install_docker(){
+    if command -v docker &> /dev/null; then
+        echo -e "${Green}Docker 已安装，进行下一步.${Font}"
+    else
+        echo -e "${Green}Docker 未安装，正在为您安装...${Font}"
+        curl -fsSL https://get.docker.com | bash -s docker
+        echo -e "${Green}Docker 安装成功！${Font}"
+    fi
+}
+
+#安装幻兽帕鲁服务端
+install_pal_server(){
+    if [ $(docker ps -a -q -f name=steamcmd) ]; then
+        echo -e "${Red}幻兽帕鲁服务端已存在，安装失败！${Font}"
+    else
+        echo -e "${Green}开始安装幻兽帕鲁服务端...${Font}"
+        docker run -dit --name steamcmd --net host miaowmint/palworld
+        echo -e "${Green}幻兽帕鲁服务端已成功安装并启动！${Font}"
+    fi
+}
+
+#启动幻兽帕鲁服务端
+start_pal_server(){
+    if [ $(docker ps -a -q -f name=steamcmd) ]; then
+        echo -e "${Green}开始启动幻兽帕鲁服务端...${Font}"
+        docker start steamcmd
+        echo -e "${Green}幻兽帕鲁服务端已成功启动！${Font}"
+    else
+        echo -e "${Red}幻兽帕鲁服务端不存在，启动失败！${Font}"
+    fi
+}
+
+#停止幻兽帕鲁服务端
+stop_pal_server(){
+    if [ $(docker ps -a -q -f name=steamcmd) ]; then
+        echo -e "${Green}开始停止幻兽帕鲁服务端...${Font}"
+        docker stop steamcmd
+        echo -e "${Green}幻兽帕鲁服务端已成功停止！${Font}"
+    else
+        echo -e "${Red}幻兽帕鲁服务端不存在，停止失败！${Font}"
+    fi
+}
+#查看幻兽帕鲁服务端状态
+check_pal_server_status(){
+    if [ $(docker ps -a -q -f name=steamcmd) ]; then
+        echo -e "${Green}幻兽帕鲁服务端状态如下：${Font}"
+        docker ps -a -f name=steamcmd
+        echo -e "${Green}幻兽帕鲁服务端资源使用情况如下：${Font}"
+        docker stats --no-stream steamcmd
+        echo -e "${Green}服务器内存使用情况如下：${Font}"
+        free -h
+    else
+        echo -e "${Red}幻兽帕鲁服务端不存在！${Font}"
+    fi
+}
+#修改服务端配置
+modify_config(){
+    if [ $(docker ps -a -q -f name=steamcmd) ]; then
+        echo -e "${Green}请前往 https://www.xuehaiwu.com/Pal/ （原脚本作者的网站）进行配置，并输入配置文件ID${Font}"
+        read -p "例如配置文件URL为 https://www.xuehaiwu.com/Pal/configs/config_1706097640.txt ，则输入1706097640" iniid
+        if [ -n "$iniid" ]; then
+            curl -o /data/palworld/PalWorldSettings.ini https://www.xuehaiwu.com/Pal/configs/config_${iniid}.txt
+        fi
+        if [ -f /data/palworld/PalWorldSettings.ini ]; then
+            echo -e "${Green}开始修改服务端配置...${Font}"
+            docker cp /data/palworld/PalWorldSettings.ini steamcmd:/home/steam/Steam/steamapps/common/PalServer/Pal/Saved/Config/LinuxServer/
+            echo -e "${Green}服务端配置已成功修改！服务端重启后生效！${Font}"
+            restart_pal_server
         else
-            echo "检测到8000端口未被占用"
-            portwillbe="8000"
+            echo -e "${Red}未找到服务端配置文件，请前往 https://www.xuehaiwu.com/Pal/ 进行配置。${Font}"
         fi
     else
-        echo -e "\n\033[31m占用情况检测失败，请确保8000端口未被其他应用占用\033[0m"
-        portwillbe="8000"
+        echo -e "${Red}幻兽帕鲁服务端不存在，修改配置失败！${Font}"
     fi
 }
 
-basicinfo(){
-    # read -p " 你的hexo是否部署在本地？回车默认为部署在本地，输入任意字符视为部署在github等托管平台 " locally
-    #     if [ -z "$locally" ];then
-    #         locally="yes"
-    #     fi
-    echo -e "\n\033[31m此脚本默认你的hexo部署在本地，如果你的hexo部署在github等托管平台，建议你使用Vercel部署，参考 https://www.oplog.cn/qexo/start/build.html （其实是因为逻辑太麻烦我懒得写）\033[0m"
-    echo -e "你是否需要镜像内置的nginx来反代qexo和hexo？（反代后可以通过域名访问qexo和hexo，\n但如果你的服务器已经安装了nginx等web服务端，或是后续有建其他网站的需求，建议你在服务器上自行反代而不是用镜像内置的nginx反代）"
-    read -p " 请注意，回车默认为使用镜像内置的nginx反代，输入任意字符视为无需反代或自行反代 " useproxy
-        if [ -z "$useproxy" ];then
-            useproxy="yes"
-        fi
-    read -p " 请输入你的qexo使用的域名，回车默认为不需要使用域名反代Qexo " qexodomain
-        if [ -z "$qexodomain" ];then
-            qexodomain="yourqexo.com"
-        fi 
-    read -p " 请输入你hexo源码的绝对路径 " hexofile
-        if [ -z "$hexofile" ];then
-            hexofile="/www/site"
-        fi 
-    if [ "$useproxy" == "yes" ];then
-        read -p " 请输入你的hexo使用的域名 " hexodomain
-            if [ -z "$hexodomain" ];then
-                hexodomain="yourhexo.com"
-            fi 
-        read -p " 请输入你的邮箱（用于申请ssl证书） " acmemail
-            if [ -z "$acmemail" ];then
-                acmemail="example@qexo.com"
-            fi 
-    fi
-    otherinfo
+#增加swap内存
+add_swap(){
+echo -e "${Green}请输入需要添加的swap，单位为 G ，例如输入8则会添加8G的SWAP${Font}"
+read -p "请输入swap数值:" swapsize
+#检查是否存在swapfile
+grep -q "swapfile" /etc/fstab
+#如果不存在将为其创建swap
+if [ $? -ne 0 ]; then
+    echo -e "${Green}swapfile未发现，正在为其创建swapfile${Font}"
+    fallocate -l ${swapsize}G /swapfile
+    chmod 600 /swapfile
+    mkswap /swapfile
+    swapon /swapfile
+    echo '/swapfile none swap defaults 0 0' >> /etc/fstab
+         echo -e "${Green}swap创建成功，并查看信息：${Font}"
+         cat /proc/swaps
+         cat /proc/meminfo | grep Swap
+else
+    echo -e "${Red}swapfile已存在，swap设置失败${Font}"
+fi
 }
 
-otherinfo(){
-    if [ "$useproxy" != "yes" ];then
-        checkport
-        qexoport=$portwillbe           #容器内的qexo映射到宿主机的端口，如使用镜像内置的nginx反代，则此项无效
-    fi
-    qexoname="qexo"                    #qexo容器的名称
-    qexofile="/data/qexo"              #后续下载并修改所需文件时将运行qexo所需的文件放置于宿主机的哪个目录
-}
-
-chickandinstall(){
-    echo -e "\033[33m接下来开始检测是否安装Docker及git\033[0m"
-    sleep 1s
-    echo "开始检测是否安装Docker......"
-    sleep 1s
-    docker -v
-    if [ $? -eq  0 ]; then
-        echo -e "\033[33m检测到Docker已安装，继续下一步\033[0m"
+#增加定时重启
+add_restart(){
+    if [ $(docker ps -a -q -f name=steamcmd) ]; then
+        echo -e "${Green}开始增加定时重启...${Font}"
+        echo -e "${Green}1、每天凌晨5点${Font}"
+        echo -e "${Green}2、每12小时（每天0点/12点）${Font}"
+        echo -e "${Green}3、自定义${Font}"
+        read -p "请输入数字 [1-3]:" num
+        case "$num" in
+            1)
+            echo "0 5 * * * cd $(pwd) && ./restart.sh" >> /etc/crontab
+            ;;
+            2)
+            echo "0 */12 * * * cd $(pwd) && ./restart.sh" >> /etc/crontab
+            ;;
+            3)
+            read -p "请输入定时重启的cron表达式:" cron
+            echo "$cron cd $(pwd) && ./restart.sh" >> /etc/crontab
+            ;;
+            *)
+            echo -e "${Red}请输入正确数字 [1-3]${Font}"
+            add_restart
+            ;;
+        esac
+        echo -e "${Green}定时重启已成功增加！${Font}"
     else
-        echo "Docker未安装，开始安装Docker环境..."
-        curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun && systemctl enable docker && systemctl start docker
-        echo -e "\033[33mDocker安装完成，继续下一步\033[0m"
+        echo -e "${Red}幻兽帕鲁服务端不存在，增加定时重启失败！${Font}"
     fi
-    echo -e "\033[33m休息5秒钟\033[0m"
-    sleep 5s
-    echo "开始检测是否安装git......"
-    sleep 1s
-    git --version
-    if [ $? -eq  0 ]; then
-        echo -e "\033[33mgit已安装\033[0m"
+}
+
+#重启幻兽帕鲁服务端
+restart_pal_server(){
+    if [ $(docker ps -a -q -f name=steamcmd) ]; then
+        echo -e "${Green}开始重启幻兽帕鲁服务端...${Font}"
+        docker start steamcmd
+        echo -e "${Green}幻兽帕鲁服务端已成功重启！${Font}"
     else
-        echo "git未安装，开始安装git..."
-        systype=`cat /etc/os-release`
-        if [[ $systype =~ "centos" ]];then
-            echo "判断为centos系统，使用yum安装git"
-            yum -y update && yum -y install git
-        else
-            echo "判断为debian/ubuntu系统，使用apt安装git"
-            apt -y update && apt -y install git
-        fi
-        echo -e "\033[33mgit安装完成，继续下一步\033[0m"
+        echo -e "${Red}幻兽帕鲁服务端不存在，重启失败！${Font}"
     fi
 }
 
-downloadandchangefile(){
-    echo -e "\033[33m接下来开始修改配置文件\033[0m"
-    sleep 1s
-    git clone https://ghproxy.com/https://github.com/Qexo/Qexo.git $qexofile
-    cd $qexofile && wget https://ghproxy.com/https://github.com/miaowmint/Qexo-docker/releases/download/1.0.1/Qexo-docker1.0.1.tar.gz && tar zxvf Qexo-docker1.0.1.tar.gz && rm Qexo-docker1.0.1.tar.gz
-    serverip=`curl -L www.loliapi.com/qexoip/`
-    echo -e "获取到服务器的IP为\033[32m$serverip\033[0m"
-    sed -i "s|127.0.0.1|$serverip|g" $qexofile/configs.py
-    sed -i "s|yourqexo.com|$qexodomain|g" $qexofile/configs.py
-    sed -i "s|yourqexo.com|$qexodomain|g" $qexofile/nginx1.conf
-    sed -i "s|yourhexo.com|$hexodomain|g" $qexofile/nginx1.conf
-    sed -i "s|/www/site|$hexofile|g" $qexofile/nginx1.conf
-    sed -i "s|yourqexo.com|$qexodomain|g" $qexofile/nginx2.conf
-    sed -i "s|yourhexo.com|$hexodomain|g" $qexofile/nginx2.conf
-    sed -i "s|/www/site|$hexofile|g" $qexofile/nginx2.conf
-    echo -e "\033[33m配置文件修改完成，休息5秒钟\033[0m"
-    sleep 5s
-}
-
-runcontainer(){
-    downloadandchangefile
-    echo -e "\033[33m接下来开始运行Qexo\033[0m"
-    sleep 1s
-    if [ "$useproxy" == "yes" ];then
-    docker run -dit --name $qexoname --hostname $qexoname --restart always -v $qexofile:/usr/local/qexo -v $hexofile:$hexofile -p 80:80 -p 443:443 -e USEPROXY=$useproxy -e QEXODOMAIN=$qexodomain -e HEXODOMAIN=$hexodomain -e ACMEMAIL=$acmemail miaowmint/qexo-docker:latest
+#删除幻兽帕鲁服务端
+delete_pal_server(){
+    if [ $(docker ps -a -q -f name=steamcmd) ]; then
+        echo -e "${Green}开始删除幻兽帕鲁服务端...${Font}"
+        docker stop steamcmd
+        docker rm steamcmd
+        echo -e "${Green}幻兽帕鲁服务端已成功删除！${Font}"
     else
-    docker run -dit --name $qexoname --hostname $qexoname --restart always -v $qexofile:/usr/local/qexo -v $hexofile:$hexofile -p $qexoport:8000 miaowmint/qexo-docker:latest
+        echo -e "${Red}幻兽帕鲁服务端不存在，删除失败！${Font}"
     fi
-    echo -e "\033[33m完成，休息5秒钟\033[0m"
-    sleep 5s
 }
 
-echoendinfo(){
-    echo -e "\n\033[34m====================================================================================================\033[0m"
-    echo -e "\n\033[33m恭喜你，安装完成了！\033[0m"
-    if [ "$useproxy" != "yes" ];then
-        sleep 5s
-        echo -e "\033[33m你现在可以访问 http://$serverip:$qexoport/ 进行初始化并愉快的使用Qexo了（记得在安全组/防火墙放通$qexoport端口）\033[0m"
-        if [ "$qexodomain" != "yourqexo.com" ];then
-            echo -e "\033[33m如果你已经设置域名反代到 http://127.0.0.1:$qexoport/ ，那么你就可以访问 http://$qexodomain/ 进行初始化并愉快的使用Qexo了\033[0m"
-        fi
+#导入幻兽帕鲁存档及配置
+import_pal_server(){
+    if [ $(docker ps -a -q -f name=steamcmd) ]; then
+        read -p "请将幻兽帕鲁存档及配置文件放入 /data/palworld 目录，然后回车继续" import
+        echo -e "${Green}开始导入幻兽帕鲁存档及配置...${Font}"
+        docker cp /data/palworld/Saved/ steamcmd:/home/steam/Steam/steamapps/common/PalServer/Pal/Saved/ 
+        restart_pal_server
+        echo -e "${Green}幻兽帕鲁存档及配置已成功导入！${Font}"
+    else
+        echo -e "${Red}幻兽帕鲁服务端不存在，导入失败！${Font}"
     fi
-    if [ "$qexodomain" != "yourqexo.com" ];then
-        sleep 5s
-        echo -e "\033[33m你现在可以访问 http://$qexodomain/ 进行初始化并愉快的使用Qexo了\033[0m"
+}
+
+#导出幻兽帕鲁存档及配置
+export_pal_server(){
+    if [ $(docker ps -a -q -f name=steamcmd) ]; then
+        echo -e "${Green}此操作会导出容器内 /home/steam/Steam/steamapps/common/PalServer/Pal/Saved 文件夹下所有的文件${Font}"
+        echo -e "${Green}导出的幻兽帕鲁存档及配置文件将会存放在 /data/palworld 目录下！${Font}"
+        echo -e "${Green}开始导出幻兽帕鲁存档及配置...${Font}"
+        mkdir -p /data/palworld
+        docker cp steamcmd:/home/steam/Steam/steamapps/common/PalServer/Pal/Saved/ /data/palworld/Saved/
+        echo -e "${Green}幻兽帕鲁存档及配置已成功导出！${Font}"
+    else
+        echo -e "${Red}幻兽帕鲁服务端不存在，导出失败！${Font}"
     fi
-    if [ "$hexodomain" != "yourhexo.com" ];then
-        sleep 5s
-        echo -e "\033[33m如果你在 $hexofile 上传了hexo源码，那你现在就可以访问 http://$hexodomain/ 访问自己的博客了\033[0m"
-    fi
-    echo -e "\033[33m关于Qexo的初始化配置可参考 https://www.oplog.cn/qexo/configs/provider.html \033[0m"
-    echo -e "\n\033[34m====================================================================================================\033[0m"
+}
+
+
+#开始菜单
+main(){
+root_need
+ovz_no
+install_docker
+clear
+echo -e "———————————————————————————————————————"
+echo -e "${Red}由于此脚本为赶工做出的，如发现脚本有任何bug或逻辑问题或改进方案，请发邮件到 cat@acat.email 联系我${Font}"
+echo -e "———————————————————————————————————————"
+echo -e "${Red}后续管理幻兽帕鲁服务端，只需要在命令行输入\033[32m palworld \033[0m即可${Font}"
+echo -e "———————————————————————————————————————"
+echo -e "${Green}1、安装幻兽帕鲁服务端${Font}"
+echo -e "${Green}2、启动幻兽帕鲁服务端${Font}"
+echo -e "${Green}3、停止幻兽帕鲁服务端${Font}"
+echo -e "${Green}4、修改服务端配置${Font}"
+echo -e "${Green}5、增加swap内存${Font}"
+echo -e "${Green}6、增加定时重启${Font}"
+echo -e "${Green}7、重启幻兽帕鲁服务端${Font}"
+echo -e "${Green}8、导入幻兽帕鲁存档及配置${Font}"
+echo -e "${Green}9、导出幻兽帕鲁存档及配置${Font}"
+echo -e "${Green}10、查看幻兽帕鲁服务端状态${Font}"
+echo -e "${Green}11、删除幻兽帕鲁服务端${Font}"
+echo -e "———————————————————————————————————————"
+read -p "请输入数字 [1-11]:" num
+case "$num" in
+    1)
+    install_pal_server
+    ;;
+    2)
+    start_pal_server
+    ;;
+    3)
+    stop_pal_server
+    ;;
+    4)
+    modify_config
+    ;;
+    5)
+    add_swap
+    ;;
+    6)
+    add_restart
+    ;;
+    7)
+    restart_pal_server
+    ;;
+    8)
+    import_pal_server
+    ;;
+    9)
+    export_pal_server
+    ;;
+    10)
+    check_pal_server_status
+    ;;
+    11)
+    delete_pal_server
+    ;;
+    *)
+    clear
+    echo -e "${Green}请输入正确数字 [1-11]${Font}"
     sleep 2s
-    echo -e "\n\033[33m你可以运行 docker logs $qexoname 以查看Qexo容器的运行日志\033[0m"
-    echo -e "\n\033[31m如果容器启动失败，日志中出现类似于 no moudle named "XXX" 的错误，大概率是因为 Qexo 作者更新新版本引用了新的模块；你可以自己通过 Dockerfile 构建新的镜像，或者是给 loli#iloli.love ( # 换成 @ ) 发送邮件告知我，我会尽快构建一个新的镜像并上传\033[0m"
-    echo -e "\n\033[33m如果Qexo使用有问题，请在此提交issue: https://github.com/Qexo/Qexo/issues/new/choose/ \033[0m"
-    echo -e "\n\033[33m如果本一键脚本使用有问题，请在此提交issue: https://github.com/miaowmint/Qexo-docker/issues/new/ \033[0m"
+    main
+    ;;
+    esac
 }
-
-
-echoinfo
-basicinfo
-chickandinstall
-runcontainer
-echoendinfo
+main
